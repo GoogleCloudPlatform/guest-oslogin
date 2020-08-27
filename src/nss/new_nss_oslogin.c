13898825a236b19f5abd8379b41e686484928527
Liam Hopkins <liamh@google.com>
2020-08-27 11:20:55 -0700

    new client component and tests

---
+++ src/nss/new_nss_oslogin.c
@@ -0,0 +1,159 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <nss.h>
+#include <pwd.h>
+#include <grp.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <pthread.h>
+
+
+#define MAX_GR_MEM 100
+
+#define PW_NAME 0
+#define PW_PASSWD 1
+#define PW_UID 2
+#define PW_GID 3
+#define PW_GECOS 4
+#define PW_DIR 5
+#define PW_SHELL 6
+#define PW_END 7
+
+#define GR_NAME 0
+#define GR_PASSWD 1
+#define GR_GID 2
+#define GR_MEM 3
+#define GR_END 4
+
+#define LEN(index) ((fields[index+1] - fields[index]) - 1)
+
+#define COPYINT(index, result) \
+    do { \
+      memset(buffer,0,buflen); \
+      memcpy(buffer,&str[fields[index]],LEN(index)); \
+      buffer[LEN(index)+1] = '\0'; \
+      result = atoi(buffer); \
+    } while(0)
+
+#define COPYSTR(index, result) \
+    do { \
+      result = buffer; \
+      memcpy(buffer, &str[fields[index]], LEN(index)); \
+      buffer[LEN(index)+1] = '\0'; \
+      buffer += LEN(index)+1; \
+    } while(0)
+
+#define DEBUGF(...) \
+    do { \
+      fprintf (stderr, __VA_ARGS__); \
+    } while(0)
+
+int parsepasswd(char *str, struct passwd *result, char *buffer, size_t buflen) {
+  int fields[PW_END+1] = {0};
+
+  fields[PW_END] = strlen(str)+1;
+  if (fields[PW_END] > (int)buflen) {
+    return ERANGE;
+  }
+
+  int i, field;
+  for(field = 1, i = 0; i < fields[PW_END]; i++) {
+    if (str[i] == ':') {
+      fields[field++] = i+1;
+    }
+  }
+
+  if (field != PW_END) {
+    return ENOENT;
+  }
+
+  COPYINT(PW_UID, result->pw_uid);
+  COPYINT(PW_GID, result->pw_gid);
+
+  memset(buffer, 0, fields[PW_END]);
+  COPYSTR(PW_NAME, result->pw_name);
+  COPYSTR(PW_PASSWD, result->pw_passwd);
+  COPYSTR(PW_GECOS, result->pw_gecos);
+  COPYSTR(PW_DIR, result->pw_dir);
+  COPYSTR(PW_SHELL, result->pw_shell);
+
+  return 0;
+}
+
+int parsegroup(char *str, struct group *result, char *buffer, size_t buflen) {
+  int fields[GR_END+1] = {0};
+  int members[MAX_GR_MEM] = {0};
+  int i, field, len;
+  char **bufp;
+
+  // Check whether buffer can fit the string.
+  fields[GR_END] = strlen(str)+1;
+  if (fields[GR_END] > (int)buflen) {
+    return ERANGE;
+  }
+
+  // Record field indexes.
+  for(field = 1, i = 0; i < fields[GR_END]; i++) {
+    if (str[i] == ':') {
+      fields[field++] = i+1;
+    }
+  }
+
+  // Wrong number of fields in record.
+  if (field != GR_END) {
+    return ENOENT;
+  }
+
+  // Record member indexes.
+  members[0] = fields[GR_MEM];
+  for(field = 1, i = fields[GR_MEM]; i < fields[GR_END]; i++) {
+    if (str[i] == ',') {
+      members[field++] = i+1;
+    }
+  }
+  members[field] = fields[GR_END];
+
+  // Check whether the buffer can fit the char* array.
+  if ((fields[GR_END] + ((field+1) * sizeof(char *))) > buflen) {
+    return ERANGE;
+  }
+
+  COPYINT(GR_GID, result->gr_gid);
+
+  memset(buffer, 0, fields[GR_END]);
+  COPYSTR(GR_NAME, result->gr_name);
+  COPYSTR(GR_PASSWD, result->gr_passwd);
+
+  result->gr_mem = bufp = (char **)buffer;
+  buffer += (sizeof(char *) * (field + 1));
+
+  for(i = 0; i < field; i++) {
+    len = ((members[i+1] - members[i]) - 1);
+    memcpy(buffer, &str[members[i]], len);
+    buffer[len+1] = '\0';
+
+    *(bufp++) = buffer;
+    buffer += len+1;
+  }
+  *bufp = NULL;
+
+  return 0;
+}

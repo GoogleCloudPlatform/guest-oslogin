8f600c82adfdf290dea2b2b1d48d4a22827d0ba8
Leandro (Leo) Dorileo <dorileo@google.com>
2023-09-04 06:07:31 +0000

    authorize keys: update to use new APIs
    Update both authorized_keys and authorized_keys_sk to use new sys logger
    facilities as well as AuthorizeUser().

--- src/authorized_keys/authorized_keys.cc
+++ src/authorized_keys/authorized_keys.cc
@@ -1,4 +1,4 @@
-// Copyright 2017 Google Inc. All Rights Reserved.
+// Copyright 2023 Google Inc. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <cstdlib>
 #include <iostream>
-#include <sstream>
-#include <string>
 
 #include <signal.h>
 
@@ -23,74 +20,62 @@
 
 using std::cout;
 using std::endl;
-using std::string;
 
-using oslogin_utils::HttpGet;
-using oslogin_utils::ParseJsonToSuccess;
-using oslogin_utils::ParseJsonToEmail;
+using oslogin_utils::AuthOptions;
+using oslogin_utils::AuthorizeUser;
+using oslogin_utils::CloseSysLog;
+using oslogin_utils::FileName;
 using oslogin_utils::ParseJsonToSshKeys;
-using oslogin_utils::UrlEncode;
-using oslogin_utils::kMetadataServerUrl;
+using oslogin_utils::SetupSysLog;
+using oslogin_utils::SysLogErr;
 
-void sigpipe_handler(int signo) {
-  // exit 0 so SSHD can use what we've already written out.
+#define SYSLOG_IDENT "sshd"
+#define SUCCESS 0
+#define FAIL    1
+
+void signal_handler(int signo) {
   _Exit(0);
 }
 
-int main(int argc, char* argv[]) {
-  if (argc != 2) {
-    cout << "usage: authorized_keys [username]" << endl;
-    return 1;
-  }
-
-  struct sigaction newact;
-  newact.sa_handler = sigpipe_handler;
-  sigemptyset(&newact.sa_mask);
-  newact.sa_flags = 0;
-  if (sigaction(SIGPIPE, &newact, NULL) == -1) {
-    cout << "Unable to add SIGPIPE handler, exiting" << endl;
-    return 1;
-  }
-
-  std::stringstream url;
-  url << kMetadataServerUrl << "users?username=" << UrlEncode(argv[1]);
+int main(int argc, char *argv[]) {
+  struct AuthOptions opts;
+  struct sigaction sig;
+  char *user_name;
   string user_response;
-  long http_code = 0;
-  if (!HttpGet(url.str(), &user_response, &http_code) ||
-      user_response.empty() || http_code != 200) {
-    if (http_code == 404) {
-      // Return 0 if the user is not an oslogin user. If we returned a failure
-      // code, we would populate auth.log with useless error messages.
-      // This exits successfully but prints no keys.
-      return 0;
+  const char *progname = FileName(argv[0]);
+
+  SetupSysLog(SYSLOG_IDENT, progname);
+
+  if (argc != 2) {
+    SysLogErr("usage: %s [username]", progname);
+    goto fail;
+  }
+
+  sig = { 0 };
+  sig.sa_handler = signal_handler;
+  sigemptyset(&sig.sa_mask);
+
+  if (sigaction(SIGPIPE, &sig, NULL) == -1) {
+    SysLogErr("Unable to initialize signal handler. Exiting.");
+    goto fail;
+  }
+
+  user_name = argv[1];
+  opts = { 0 };
+
+  if (AuthorizeUser(user_name, opts, &user_response)) {
+    // At this point, we've verified the user can log in. Grab the ssh keys from
+    // the user response.
+    std::vector<string> ssh_keys = ParseJsonToSshKeys(user_response);
+    for (size_t i = 0; i < ssh_keys.size(); i++) {
+      cout << ssh_keys[i] << endl;
     }
-    return 1;
   }
-  string email;
-  if (!ParseJsonToEmail(user_response, &email) || email.empty()) {
-    return 1;
-  }
-  // Redundantly verify that this user has permission to log in to this VM.
-  // Normally the PAM module determines this, but in the off chance a transient
-  // error causes the PAM module to permit a user without login permissions,
-  // perform the same check here. If this fails, we can guarantee that we won't
-  // accidentally allow a user to log in without permissions.
-  url.str("");
-  url << kMetadataServerUrl << "authorize?email=" << UrlEncode(email)
-      << "&policy=login";
-  string auth_response;
-  if (!HttpGet(url.str(), &auth_response, &http_code) || http_code != 200 ||
-      auth_response.empty()) {
-    return 1;
-  }
-  if (!ParseJsonToSuccess(auth_response)) {
-    return 1;
-  }
-  // At this point, we've verified the user can log in. Grab the ssh keys from
-  // the user response.
-  std::vector<string> ssh_keys = ParseJsonToSshKeys(user_response);
-  for (size_t i = 0; i < ssh_keys.size(); i++) {
-    cout << ssh_keys[i] << endl;
-  }
-  return 0;
+
+  CloseSysLog();
+  return SUCCESS;
+
+fail:
+  CloseSysLog();
+  return FAIL;
 }
